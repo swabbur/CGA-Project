de2f82869511f1e995ec752c3e5ecfe804ae719d
Wessel van de Brug <wvdbrug@gmail.com>
2021-03-17 00:55:30 +0100

    Replace individual uniforms with structs

--- res/shaders/fragment.glsl
+++ res/shaders/fragment.glsl
@@ -1,20 +1,26 @@
 #version 450 core
 
+struct PointLight {
+    vec3 diffuse;
+    vec3 specular;
+    vec3 position;
+};
+
+struct Component {
+    bool textured;
+    sampler2D sampler;
+    vec3 color;
+};
+
+struct Material {
+    Component diffuse;
+    Component specular;
+    float shininess;
+};
+
 layout(location = 1) uniform vec3 camera_position;
-
-layout(location = 2) uniform vec3 light_color_diffuse;
-layout(location = 3) uniform vec3 light_color_specular;
-layout(location = 4) uniform vec3 light_position;
-
-layout(location = 5) uniform bool material_diffuse_textured;
-layout(location = 6) uniform sampler2D material_diffuse_sampler;
-layout(location = 7) uniform vec3 material_diffuse_color;
-
-layout(location = 8) uniform bool material_specular_textured;
-layout(location = 9) uniform sampler2D material_specular_sampler;
-layout(location = 10) uniform vec3 material_specular_color;
-
-layout(location = 11) uniform float material_shininess;
+layout(location = 2) uniform PointLight light;
+layout(location = 5) uniform Material material;
 
 layout(location = 0) in vec3 fragment_position;
 layout(location = 1) in vec3 fragment_normal;
@@ -30,11 +36,11 @@ vec3 compute_diffuse_color(vec3 normal, vec3 light_direction, float light_streng
     float diffuse_strength = max(0.0, dot(normal, light_direction)) / PI;
 
     // Compute diffuse color
-    if (material_diffuse_textured) {
-        vec3 sampled_color = vec3(texture(material_diffuse_sampler, fragment_texture_coord));
-        return light_strength * diffuse_strength * light_color_diffuse * sampled_color;
+    if (material.diffuse.textured) {
+        vec3 sampled_color = vec3(texture(material.diffuse.sampler, fragment_texture_coord));
+        return light_strength * diffuse_strength * light.diffuse * sampled_color;
     } else {
-        return light_strength * diffuse_strength * light_color_diffuse * material_diffuse_color;
+        return light_strength * diffuse_strength * light.diffuse * material.diffuse.color;
     }
 }
 
@@ -43,14 +49,14 @@ vec3 compute_specular_color(vec3 normal, vec3 light_direction, float light_stren
     // Compute (normalized Blinn-Phong) specular strength
     vec3 view_direction = normalize(camera_position - fragment_position);
     vec3 half_vector = normalize(light_direction + view_direction);
-    float specular_strength = pow(max(0.0, dot(normal, half_vector)), 4.0 * material_shininess) / 2.0 / PI;
+    float specular_strength = pow(max(0.0, dot(normal, half_vector)), 4.0 * material.shininess) / 2.0 / PI;
 
     // Compute specular color
-    if (material_specular_textured) {
-        vec3 sampled_color = vec3(texture(material_specular_sampler, fragment_texture_coord));
-        return light_strength * specular_strength * light_color_specular * sampled_color;
+    if (material.specular.textured) {
+        vec3 sampled_color = vec3(texture(material.specular.sampler, fragment_texture_coord));
+        return light_strength * specular_strength * light.specular * sampled_color;
     } else {
-        return light_strength * specular_strength * light_color_specular * material_specular_color;
+        return light_strength * specular_strength * light.specular * material.specular.color;
     }
 }
 
@@ -59,9 +65,9 @@ void main() {
     // Compute shared variables
     vec3 normal = normalize(fragment_normal);
 
-    float light_distance = distance(light_position, fragment_position);
+    float light_distance = distance(light.position, fragment_position);
     float light_strength = 1.0 / 4.0 / PI / light_distance / light_distance;
-    vec3 light_direction = (light_position - fragment_position) / light_distance;
+    vec3 light_direction = (light.position - fragment_position) / light_distance;
 
     // Compute individual colors
     vec3 diffuse_color = compute_diffuse_color(normal, light_direction, light_strength);
