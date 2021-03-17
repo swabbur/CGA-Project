35e2818dd6fca51a71768253cfb74f054cf46fd5
Wessel van de Brug <wvdbrug@gmail.com>
2021-03-17 12:08:42 +0100

    Ensure GLFW window is created on the event thread

--- src/devices/DeviceManager.cpp
+++ src/devices/DeviceManager.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 #include <mutex>
 #include <stdexcept>
+#include <future>
 
 std::mutex mutex;
 DeviceManager * device_manager = nullptr;
@@ -22,103 +23,114 @@ DeviceManager::DeviceManager() : handle(nullptr), keyboard(), mouse(), window(),
         throw std::runtime_error("Could not initialize GLFW");
     }
 
-    // Set default window hints
-    glfwDefaultWindowHints();
-
-    // Set window look-and-feel
-    glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-    glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
-
-    // Set OpenGL preferences
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
-
-    // Set MSAA preferences
-    glfwWindowHint(GLFW_SAMPLES, 4);
-
-    // Compute initial width and height
-    GLFWmonitor * monitor = glfwGetPrimaryMonitor();
-    GLFWvidmode const * video_mode = glfwGetVideoMode(monitor);
-    int width = video_mode->width;
-    int height = video_mode->height;
-    if (16 * width >= 9 * height) {
-        height /= 2;
-        width = 16 * height / 9;
-    } else {
-        width /= 2;
-        height = 9 * width / 16;
-    }
-
-    // Create window
-    handle = glfwCreateWindow(width, height, "Legacy", nullptr, nullptr);
-    if (handle == nullptr) {
-        throw std::runtime_error("Could not create GLFW window");
-    }
-    window.resize(width, height);
-
-    // Pass handle to devices
-    keyboard.set_handle(handle);
-    mouse.set_handle(handle);
-    window.set_handle(handle);
-
-    // Configure callbacks
-    glfwSetWindowCloseCallback(static_cast<GLFWwindow *>(handle), [](GLFWwindow * /* window */) {
-        device_manager->window.close();
-    });
-
-    glfwSetWindowSizeCallback(static_cast<GLFWwindow *>(handle), [](GLFWwindow * /* window */, int width, int height) {
-        device_manager->window.resize(width, height);
-    });
-
-    glfwSetKeyCallback(static_cast<GLFWwindow *>(handle), [](GLFWwindow * /* window */, int key, int /* scancode */, int action, int /* mods */) {
-        switch (action) {
-
-            case GLFW_PRESS:
-                device_manager->keyboard.press(key);
-                break;
-
-            case GLFW_RELEASE:
-                device_manager->keyboard.release(key);
-                break;
-
-            default:
-                break;
-        }
-   });
-
-    glfwSetMouseButtonCallback(static_cast<GLFWwindow *>(handle), [](GLFWwindow * /* window */, int button, int action, int /* mods */) {
-        switch (action) {
-
-            case GLFW_PRESS:
-                device_manager->mouse.press(button);
-                break;
-
-            case GLFW_RELEASE:
-                device_manager->mouse.release(button);
-                break;
-
-            default:
-                break;
-        }
-    });
-
-    glfwSetCursorPosCallback(static_cast<GLFWwindow *>(handle), [](GLFWwindow * /* window */, double x, double y) {
-        device_manager->mouse.move(static_cast<float>(x), static_cast<float>(y));
-    });
-
-    // Enable raw mouse input
-    if (glfwRawMouseMotionSupported()) {
-        glfwSetInputMode(static_cast<GLFWwindow *>(handle), GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
-    }
-
     // Create event-loop
+    std::promise<std::string> promise;
+
     running = true;
-    thread = std::thread([this]() {
+    thread = std::thread([this, &promise]() {
+
+        // Set default window hints
+        glfwDefaultWindowHints();
+
+        // Set window look-and-feel
+        glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+
+        // Set OpenGL preferences
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+
+        // Set MSAA preferences
+        glfwWindowHint(GLFW_SAMPLES, 4);
+
+        // Compute initial width and height
+        GLFWmonitor * monitor = glfwGetPrimaryMonitor();
+        GLFWvidmode const * video_mode = glfwGetVideoMode(monitor);
+        int width = video_mode->width;
+        int height = video_mode->height;
+        if (16 * width >= 9 * height) {
+            height /= 2;
+            width = 16 * height / 9;
+        } else {
+            width /= 2;
+            height = 9 * width / 16;
+        }
+
+        // Create window
+        handle = glfwCreateWindow(width, height, "Legacy", nullptr, nullptr);
+        if (handle == nullptr) {
+            throw std::runtime_error("Could not create GLFW window");
+        }
+        window.resize(width, height);
+
+        // Pass handle to devices
+        keyboard.set_handle(handle);
+        mouse.set_handle(handle);
+        window.set_handle(handle);
+
+        // Configure callbacks
+        glfwSetWindowCloseCallback(static_cast<GLFWwindow *>(handle), [](GLFWwindow * /* window */) {
+            device_manager->window.close();
+        });
+
+        glfwSetWindowSizeCallback(static_cast<GLFWwindow *>(handle), [](GLFWwindow * /* window */, int width, int height) {
+            device_manager->window.resize(width, height);
+        });
+
+        glfwSetKeyCallback(static_cast<GLFWwindow *>(handle), [](GLFWwindow * /* window */, int key, int /* scancode */, int action, int /* mods */) {
+            switch (action) {
+
+                case GLFW_PRESS:
+                    device_manager->keyboard.press(key);
+                    break;
+
+                case GLFW_RELEASE:
+                    device_manager->keyboard.release(key);
+                    break;
+
+                default:
+                    break;
+            }
+        });
+
+        glfwSetMouseButtonCallback(static_cast<GLFWwindow *>(handle), [](GLFWwindow * /* window */, int button, int action, int /* mods */) {
+            switch (action) {
+
+                case GLFW_PRESS:
+                    device_manager->mouse.press(button);
+                    break;
+
+                case GLFW_RELEASE:
+                    device_manager->mouse.release(button);
+                    break;
+
+                default:
+                    break;
+            }
+        });
+
+        glfwSetCursorPosCallback(static_cast<GLFWwindow *>(handle), [](GLFWwindow * /* window */, double x, double y) {
+            device_manager->mouse.move(static_cast<float>(x), static_cast<float>(y));
+        });
+
+        // Enable raw mouse input
+        if (glfwRawMouseMotionSupported()) {
+            glfwSetInputMode(static_cast<GLFWwindow *>(handle), GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+        }
+
+        promise.set_value("");
+
         while (running) {
             glfwWaitEventsTimeout(0.1);
         }
         device_manager->window.close();
     });
+
+    std::future<std::string> future_error_message = promise.get_future();
+    std::string error_message = future_error_message.get();
+    if (!error_message.empty()) {
+        throw std::runtime_error(error_message);
+    }
 
     // Poll each device once as initialization
     keyboard.poll();
